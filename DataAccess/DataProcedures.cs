946407203dd4f603d2f93ad2a4ae96d98e3c581b
gGeo93 <giorgosfilippas93@gmail.com>
2024-04-12 13:53:45 +0300

    test2

--- DataAccess/DataProcedures.cs
+++ DataAccess/DataProcedures.cs
@@ -12,91 +12,7 @@ namespace DataAccess
         private string JsonData { get; set; } = string.Empty;
         private string ErrorMessage { get; set; } = string.Empty;
         
-        #region GetDataAccess
-        
-        public string FetchData(string url)
-        {
-            try 
-            {
-                using(var client = new HttpClient())
-                {
-                    var request = new HttpRequestMessage(HttpMethod.Get, url);
-                    HttpResponseMessage responseMessage = client.Send(request);
-                    JsonData = responseMessage.Content.ReadAsStringAsync().Result;
-                }
-                return JsonData;
-            }
-            catch(Exception ex) 
-            {
-                JsonData = $"There was an error: {ex.Message}";
-                return JsonData;
-            }
-        }
-
-
-        public string SendDataToFront()
-        {
-            return JsonData;
-        }
-        #endregion
-
-        #region PostDataAccess
-        public void PostData(string url, string dataToPost)
-        {
-            try 
-            { 
-                using(var client = new HttpClient()) 
-                {
-                    var request = new HttpRequestMessage(HttpMethod.Post, url);
-                    request.Content = new StringContent(dataToPost, Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = client.Send(request);                
-                }
-            }
-            catch 
-            {
-                throw;
-            }
-        }
-
-        #endregion
-
-        #region PutDataAccess
-        public void PutData(string url, string dataToPut)
-        {
-            try 
-            {
-                using(var client = new HttpClient()) 
-                { 
-                    var request = new HttpRequestMessage(HttpMethod.Put, url);
-                    request.Content = new StringContent(dataToPut, Encoding.UTF8, "application/json");
-                    HttpResponseMessage response = client.Send(request);
-                }
-            }
-            catch 
-            {
-                throw;
-            }
-        }
-
-        #endregion
-
-        #region DeleteDataAccess
-        public void DeleteData(string url)
-        {
-            try 
-            { 
-                using(var client = new HttpClient()) 
-                { 
-                    var request = new HttpRequestMessage(HttpMethod.Delete, url);
-                    var response = client.Send(request);
-                }                
-            }
-            catch 
-            {
-                throw;
-            }
-        }
-
+      
         public string CrudOperation(string url, string? dataToSend, HttpMethod httpMethod)
         {
             try 
@@ -117,7 +33,5 @@ namespace DataAccess
                 return ErrorMessage;
             }
         }
-
-        #endregion
     }
 }
