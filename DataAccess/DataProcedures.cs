9eec384011a6566ccd28518a30e8b6a99f799948
gGeo93 <giorgosfilippas93@gmail.com>
2024-04-27 09:21:26 +0300

    clean up

--- DataAccess/DataProcedures.cs
+++ DataAccess/DataProcedures.cs
@@ -4,40 +4,43 @@ namespace DataAccess;
 
 public class DataProcedures : IDataProcedures
 {
-    private string JsonData { get; set; } = string.Empty;        
+    private string JsonData { get; set; } = string.Empty;
+    private HttpResponseMessage? Response { get; set; } = new HttpResponseMessage();
   
     public async Task<string> CrudOperation(string url, string? dataToSend, HttpMethod httpMethod)
     {
         try
         {
-            HttpResponseMessage response;
-            using (var client = new HttpClient())
-            {
-                response = await HttpClientCall(url, dataToSend, httpMethod, client);
-            }
-            return response.IsSuccessStatusCode ? JsonData : response.ReasonPhrase!; 
+            Response = await HttpClientCall(url, dataToSend, httpMethod);
+            
+            return Response.IsSuccessStatusCode ? JsonData : Response.ReasonPhrase!; 
         }
         catch (Exception ex)
         {
             return ex.Message;
         }
+        finally
+        {
+            Response?.Dispose();       
+        }
     }
 
-    private async Task<HttpResponseMessage> HttpClientCall(string url, string? dataToSend, HttpMethod httpMethod, HttpClient client)
+    private async Task<HttpResponseMessage> HttpClientCall(string url, string? dataToSend, HttpMethod httpMethod)
     {
-        HttpResponseMessage response;
-
-        JsonData = string.Empty;
+        using (var client = new HttpClient())
+        {            
+            JsonData = string.Empty;
         
-        var request = new HttpRequestMessage(httpMethod, url);
+            var request = new HttpRequestMessage(httpMethod, url);
         
-        if (!string.IsNullOrEmpty(dataToSend))
-            request.Content = new StringContent(dataToSend, Encoding.UTF8, "application/json");
+            if (!string.IsNullOrEmpty(dataToSend))
+                request.Content = new StringContent(dataToSend, Encoding.UTF8, "application/json");
 
-        response = await client.SendAsync(request);
+            var response = await client.SendAsync(request);
 
-        JsonData = await response.Content.ReadAsStringAsync();
+            JsonData = await response.Content.ReadAsStringAsync();
         
-        return response;
+            return response;
+        }
     }
 }
