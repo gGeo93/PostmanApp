fd8bf895a11891cfdcabbf0add34932cc85d44d9
Georgios Filippas <53982029+gGeo93@users.noreply.github.com>
2024-05-23 06:43:34 +0300

    Delete PostmanBlazorWebeAssembly directory

--- PostmanBlazorWebeAssembly/UIModel/RequestModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PostmanWebAssembly.UIModel;
-
-public class RequestModel
-{
-    public string Url { get; set; } = string.Empty;
-    public string Method { get; set; } = string.Empty;
-    public string DataToSend { get; set; } = string.Empty;
-}
